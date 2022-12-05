86cb270dd1b9ba18af8ce8bc6651ba5c32755aa2
Adrian Bach <adrian.bach@stir.ac.uk>
2022-12-05 11:16:09 +0000

    in the shape of an experiment varying predInit for the baseline params

--- IncreasePredInit/intraSpecComp-AvgOffspring-launchReplicatesSim.sh
+++ IncreasePredInit/intraSpecComp-AvgOffspring-launchReplicatesSim.sh
@@ -35,7 +35,7 @@ pry_move_2=0.1  # argv[10] prey 2 movement range
 # pry_surv_1=3  # argv[13] prey 1 resource units needed to pass survival trial
 # pry_surv_2=3  # argv[14] prey 2 resource units needed to pass survival trial
 pry_offs_1=1    # argv[15] prey 1 max number of offspring
-# pry_offs_2=1    # argv[16] prey 2 max number of offspring
+pry_offs_2=1    # argv[16] prey 2 max number of offspring
 # pry_repr_1=5  # argv[17] prey 1 resource units needed to pass reproduction trial
 # pry_repr_2=5  # argv[18] prey 2 resource units needed to pass reproduction trial
 # pry_intro_1=0   # argv[19]
@@ -43,7 +43,7 @@ pry_offs_1=1    # argv[15] prey 1 max number of offspring
 
 # predator variables
 prd_nb=1        # argv[21] number of predator types
-prd_init_1=200    # argv[22] predator 1 initial density in nb of individuals
+# prd_init_1=25    # argv[22] predator 1 initial density in nb of individuals
 prd_move_1=0.1  # argv[23] predator 1 max movement range in fraction of size
 prd_offs_1=1    # argv[26] predator 1 max number of offspring
 prd_intr_1=201    # argv[28] predator 1 time of introduction in the model
@@ -93,8 +93,8 @@ make
 
 #### Simulation loop ####
 
-ratioArray=($(seq 1 1 10))
-# ratioArray=(1 1.1 1.5 2 2.5 3 5)
+# ratioArray=($(seq 1 1 10))
+ratioArray=(2 4 6 8 10 14)
 # spcfArray=(0 1 0)
 # oprtArray=(0 0 1) 
 
@@ -116,12 +116,12 @@ ratioArray=($(seq 1 1 10))
     # loop over prdCatchProb
     for ((k=0 ; k<${#ratioArray[@]} ; k++))
     do
-        pry_offs_2=`echo "scale=0; ${ratioArray[$k]}*$pry_offs_1" | bc`;
+        prd_init_1=`echo "scale=0; ${ratioArray[$k]}*$prd_init_1" | bc`;
 
-        echo "predators specific: $prd_spcf_1 ; predators oportunist: $prd_oprt_1 ; prey2 to prey1 average offspring nb ratio : ${ratioArray[$k]}"
+        echo "predators specific: $prd_spcf_1 ; predators oportunist: $prd_oprt_1 ; predator initial population: $(($prd_init_1*${ratioArray[$k]}))"
 
         # name the simulation with only the variables of interest and their value
-        sim_name="predInit200-pryOffsRatio${ratioArray[$k]}" # argv[1]
+        sim_name="predInitTest-prdInitRatio${ratioArray[$k]}" # argv[1]
 
         #echo "prey populations initial density are $pry_init_1 and $pry_init_2 and predators $prd_init_1"
 
